7260a6df7360609c62fd1ceda5e4347ba2486593
jason <jason@zgwit.com>
2021-11-18 23:45:24 +0800

    小改，使用cJSON替换，ArduinoJSON，主要是通用性 和 效率问题

--- define.hpp
+++ define.hpp
@@ -3,8 +3,6 @@
 #include <stdint.h>
 #include <stddef.h>
 
-#include <cstdint>
-
 #include <memory>
 #include <string>
 #include <vector>
@@ -14,18 +12,16 @@
 #include <numeric>
 #include <algorithm>
 
+#include "cjson/cJSON.h"
+
 
 #define Pointer std::shared_ptr
 
 
-#define ARDUINOJSON_ENABLE_STD_STRING
-//#include <ArduinoJson.hpp>
-#include <ArduinoJson.h> //°üº¬using namespace ArduinoJson
-
 class Proto
 {
 public:
-    virtual bool Parse(JsonObject& obj) = 0;
+    virtual bool Parse(cJSON* json) = 0;
 };
 
 
@@ -36,3 +32,6 @@ typedef struct RangeTime {
 
 typedef uint16_t RangeWeek;
 #define testRangeWeek(r,d) ((0x1<<d)&r)
+
+
+
