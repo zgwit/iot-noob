7fc628e4474ebffb5f76287f3a532afdd6c28f66
Jason <jason@zgwit.com>
2021-11-30 17:04:17 +0800

    AppProfile删除

--- app.cpp
+++ app.cpp
@@ -1,37 +1,61 @@
 #include "app.hpp"
 
-bool AppProfile::Parse(cJSON* json)
-{
-	if (!json) return false;
 
-	json_member_get_string(this, json, name);
-	json_member_get_string(this, json, version);
-
-	json_get_object_array(this, json, devices);
-	json_get_object_array(this, json, variables);
-	json_get_object_array(this, json, aggregators);
-	json_get_object_array(this, json, commands);
-	json_get_object_array(this, json, alarms);
-	json_get_object_array(this, json, jobs);
-	json_get_object_array(this, json, reactors);
-
-	return true;
-}
-
-
-
-App::App(AppProfile* profile)
+App::App()
 {
 }
 
 App::~App()
 {
 	for (auto& it : devices) delete it;
-	for (auto& it : variables) delete it;
+	//for (auto& it : variables) delete it;
 	for (auto& it : aggregators) delete it;
-	for (auto& it : commands) delete it.second;
+	for (auto& it : commands) delete it;
 	for (auto& it : alarms) delete it;
 	for (auto& it : jobs) delete it;
 	for (auto& it : reactors) delete it;
+}
+
+
+void App::Load(cJSON* json)
+{
+    json_array_foreach(json_get(json, "devices"), item) {
+        auto d = new Device();
+        d->Load(item, this);
+        devices.push_back(d);
+        //TODO ¼ÓÔØÅäÖÃÎÄ¼þ
+    }
+    json_array_foreach(json_get(json, "variables"), item) {
+        auto var = new Variable();
+        var->Load(item);
+        context.Set(var->name, var);
+    }
+    json_array_foreach(json_get(json, "aggregators"), item) {
+        auto c = new Aggregator();
+        c->Load(item);
+        aggregators.push_back(c);
+    }
+    json_array_foreach(json_get(json, "commands"), item) {
+        auto c = new Command();
+        c->Load(item, this, nullptr);
+        commands.push_back(c);
+        commandsIndex[c->name] = c;
+    }
+    json_array_foreach(json_get(json, "alarms"), item) {
+        auto a = new Alarm();
+        a->Load(item, context);
+        alarms.push_back(a);
+    }
+    json_array_foreach(json_get(json, "jobs"), item) {
+        auto j = new Job();
+        j->Load(item, this, nullptr);
+        jobs.push_back(j);
+    }
+    json_array_foreach(json_get(json, "reactors"), item) {
+        auto r = new Reactor();
+        r->Load(item, context);
+        reactors.push_back(r);
+    }
+
 }
 
--- app.hpp
+++ app.hpp
@@ -11,39 +11,26 @@
 #include "job.hpp"
 #include "reactor.hpp"
 
-class AppProfile {
-public:
-    std::string name;
-    std::string version;
-
-    std::vector<DeviceProfile> devices;
-    std::vector<VariableProfile> variables;
-    std::vector<AggregatorProfile> aggregators;
-    std::vector<CommandProfile> commands;
-    std::vector<AlarmProfile> alarms;
-    std::vector<ReactorProfile> reactors; 
-    std::vector<JobProfile> jobs;
-
-    bool Parse(cJSON* json);
-};
 
 class App
 {
 private:
-    //AppProfile* profile;
     Context context;
 
     std::vector<Device*> devices;
-    std::vector<Variable*> variables;
+    //std::vector<Variable*> variables;
     std::vector<Aggregator*> aggregators;
-    std::map<std::string, Command*> commands;
+    std::vector<Command*> commands;
     std::vector<Alarm*> alarms;
     std::vector<Job*> jobs;
     std::vector<Reactor*> reactors;
 
+    std::map<std::string, Command*> commandsIndex;
 public:
-    App(AppProfile* profile);
+    App();
     ~App();
+
+    void Load(cJSON* json);
 
     void findDevice(const std::string& name, std::vector<Device*>& devices) {}
 };
