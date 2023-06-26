a4526052fb28ec7286416efa69facf64185457b9
jason <jason@zgwit.com>
2023-06-26 12:53:06 +0800

    修改项目名称

--- main.lua
+++ main.lua
@@ -14,14 +14,19 @@ require "net"
 ril.request("AT+RNDISCALL=0,1")
 
 -- 加载网络指示灯和LTE指示灯功能模块
--- require "netLed"
--- pmd.ldoset(2,pmd.LDO_VLCD)
--- netLed.setup(true,pio.P0_1,pio.P0_4)
+require "netLed"
+pmd.ldoset(2, pmd.LDO_VLCD)
+netLed.setup(true, pio.P0_1, pio.P0_4)
 
 -- 加载远程升级功能模块
--- PRODUCT_KEY = "S6dfiXYGBJ6EKwFTfbNWqUobYStkoVCl"
--- require "update"
--- update.request()
+PRODUCT_KEY = "pgHEBKHLTryaYpYAaGDUOBncHYPR9m1l"
+require "update"
+update.request()
+
+
+-- 加载网关程序
+
+
 
 -- 启动系统框架
 sys.init(0, 0)
