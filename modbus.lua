fd3e84dfe774922d37b7c3e12d5427f6f3f3b646
Jason <jason@zgwit.com>
2023-06-30 16:10:18 +0800

    优化

--- modbus.lua
+++ modbus.lua
@@ -20,7 +20,7 @@ function modbus:read(slave, code, addr, len)
     local ret = self.link.read(self.timeout)
     if ret == "" or ret == nil then return false end
     return string.sub(ret, 4) -- 删掉：从站，功能码，字数
-    -- return unpack(string.sub(ret, 4), mappers)
+    -- return unpack(string.sub(ret, 4), points)
 end
 
 -- 写入 1->5 3->6，仅支持单值写入
@@ -54,18 +54,18 @@ function modbus:write(slave, code, addr, data)
     return true
 end
 
-function modbus:poll(slave, poller)
-    local data = self.read(slave, poller.code, poller.addr, poller.length)
+function modbus:poll(device, mapper)
+    local data =
+        self.read(device.slave, mapper.code, mapper.addr, mapper.length)
     if not data then return false end
-    return parse(poller.code, data, poller.mappers)
+    return parse(mapper.code, data, mapper.points)
 end
 
-function modbus:put(slave, name, value, pollers)
-    local ret, code, addr, data = lookup(name, value, pollers)
+function modbus:put(device, name, value, mappers)
+    local ret, code, addr, data = lookup(name, value, mappers)
     if not ret then return false end
-    return self.write(slave, code, addr, data)
+    return self.write(device.slave, code, addr, data)
 end
-
 
 -- 数据类型
 local unpackTypes = {
@@ -84,11 +84,11 @@ local unpackTypes = {
 }
 
 -- 解析数据
-function parse(code, data, mappers)
+function parse(code, data, points)
     local result = {}
     if code == 1 or code == 2 then
         -- 取线圈或触点
-        for i, m in ipairs(mappers) do
+        for i, m in ipairs(points) do
             local str = string.sub(data, m.offset / 8)
             local val = pack.unpack(str, "b")
             local pos = m.offset % 8
@@ -96,7 +96,7 @@ function parse(code, data, mappers)
         end
     elseif code == 3 or code == 4 then
         -- 取寄存器
-        for i, m in ipairs(mappers) do
+        for i, m in ipairs(points) do
             -- {offset = 0, be = true, type = "uint16", name = "temp", rate = 0.1， adjust = 0}
             local str = string.sub(data, m.offset * 2)
             -- 拼接格式
@@ -118,25 +118,25 @@ function parse(code, data, mappers)
 end
 
 -- 定位数据 
-function lookup(name, value, pollers)
-    for i, p in ipairs(pollers) do
-        if p.code == 1 then
+function lookup(name, value, mappers)
+    for i, m in ipairs(mappers) do
+        if m.code == 1 then
             -- 写线圈
-            for i, m in ipairs(p.mappers) do
-                if m.name == name then
-                    return true, p.code, p.addr + m.offset, value
+            for i, p in ipairs(m.points) do
+                if p.name == name then
+                    return true, m.code, m.addr + p.offset, value
                 end
             end
-        elseif p.code == 3 then
+        elseif m.code == 3 then
             -- 写寄存器
-            for i, m in ipairs(p.mappers) do
-                if m.name == name then
+            for i, p in ipairs(m.points) do
+                if p.name == name then
                     -- if m.adjust ~= 0 then value = value - m.adjust end
-                    if m.rate ~= 0 and m.rate ~= 1 then
-                        value = value / m.rate
+                    if p.rate ~= 0 and p.rate ~= 1 then
+                        value = value / p.rate
                     end
-                    local fmt = (m.be and '>' or '<') .. unpackTypes[m.type]
-                    return true, p.code, p.addr + m.offset,
+                    local fmt = (p.be and '>' or '<') .. unpackTypes[p.type]
+                    return true, m.code, m.addr + p.offset,
                            pack.pack(fmt, value)
                 end
             end
